a96c41badb8115ec841566e019a76b4a19034595
UTEC-MauricioNovo <83225058+UTEC-MauricioNovo@users.noreply.github.com>
2021-08-25 23:57:22 -0300

    Add files via upload

---
+++ ejecutar_constructor_script.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+echo "Se esta ejecutando el script demostracion del pipeline"
+touch demoapp.jar
---
+++ script.sh
@@ -0,0 +1,7 @@
+echo "# PipeLinePrimerPresencial" >> README.md
+git init
+git add README.md
+git commit -m "realizando el primer commit"
+git remote add origin
+git@github.com:UTEC-MauricioNovo/PipeLinePrimerPresencial.git
+git push -u origin master
